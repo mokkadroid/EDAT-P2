ad4a71afe69401f2707ecc4f4c210abc6e6274ab
Erik-YO <72315574+Erik-YO@users.noreply.github.com>
2020-11-06 01:34:12 +0100

    Splint 1

--- include/odbc.h
+++ include/odbc.h
@@ -2,19 +2,17 @@
 #define AUX_H
 
 
-void odbc_try();
 
 
 /* CONNECTION PARAMETERS, ADAPT TO YOUR SCENARIO */
 #define CONNECTION_PARS "DRIVER=PostgreSQL ANSI;DATABASE=classicmodels;SERVER=localhost;PORT=5432;UID=alumnodb;PWD=alumnodb;"
 
-/* REPORT OF THE MOST RECENT ERROR USING HANDLE handle */
-void odbc_extract_error(char *fn, SQLHANDLE handle, SQLSMALLINT type);
+
 
 /* STANDARD CONNECTION PROCEDURE */
-int odbc_connect(SQLHENV* env, SQLHDBC* dbc);
+SQLRETURN odbc_connect(SQLHENV* env, SQLHDBC* dbc);
 
 /* STANDARD DISCONNECTION PROCEDURE */
-int odbc_disconnect(SQLHENV env, SQLHDBC dbc);
+SQLRETURN odbc_disconnect(SQLHENV env, SQLHDBC dbc);
 
 #endif
