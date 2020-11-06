ea9a79e6006d80765712c7f03d2710a91a62f56e
Erik-YO <72315574+Erik-YO@users.noreply.github.com>
2020-11-06 11:53:24 +0100

    Comentarios

--- src/menus.c
+++ src/menus.c
@@ -1,4 +1,11 @@
-
+/**
+ * @brief It implements the functions for the menus management
+ *
+ * @file menus.c
+ * @author Erik Yuste & Lucía Martinez-Valero
+ * @version 1.0
+ * @date 28-10-2020
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,26 +21,159 @@
 
 
 /*Private functions*/
+
+/**
+* menus_changeScreen Imprime una cantidad de saltos de linea suficiente para que el anterior menu desaparezca de la pantalla
+*
+* @date 28-10-2020
+* @author: Erik Yuste
+*
+* @param out Puntero a FILE donde se imprimirá
+*/
 static void menus_changeScreen(FILE *out);
+/**
+* menus_input Recoge la entrada del usuario sin que tenga que pulsar 'Enter'
+*
+* @date 28-10-2020
+* @author: Erik Yuste
+*
+* @param c Puntero a char donde se guardará la entrada del usuario
+*/
 static void menus_input(char *c);
+/**
+* menus_inputError Imprime un mensaje de error 
+*
+* @date 28-10-2020
+* @author: Erik Yuste
+*
+* @param out Puntero a FILE donde se imprimirá el mensaje
+*/
 static void menus_inputError(FILE *out);
 
+
+
+
+
+
+
 /*Submenus*/
+
+/**
+* menus_generalPrint Imprime la interfaz que ve el usuario en el menu general
+*
+* @date 28-10-2020
+* @author: Erik Yuste
+*
+* @param out Puntero a FILE donde se imprimirá el menu
+*/
 static void menus_generalPrint(FILE *out);
+/**
+* menus_productsPrint Imprime la interfaz que ve el usuario en el menu de productos
+*
+* @date 28-10-2020
+* @author: Erik Yuste
+*
+* @param out Puntero a FILE donde se imprimirá el menu
+*/
 static void menus_productsPrint(FILE *out);
+/**
+* menus_ordersPrint Imprime la interfaz que ve el usuario en el menu de pedidos
+*
+* @date W.I.P.
+* @author: 
+*
+* @param out Puntero a FILE donde se imprimirá el menu
+*/
 static void menus_ordersPrint(FILE *out);
+/**
+* menus_clientesPrint Imprime la interfaz que ve el usuario en el menu de clientes
+*
+* @date W.I.P.
+* @author: 
+*
+* @param out Puntero a FILE donde se imprimirá el menu
+*/
 static void menus_customersPrint(FILE *out);
 
 
+/**
+* menus_products Implementa la lógica del menu de productos
+*
+* @date 28-10-2020
+* @author: Erik Yuste
+*
+* @param Puntero a SQLHSTMT
+* @param out Puntero a FILE donde se imprimirá el menu
+* @return Devuelve 0 si no ha habido ningún error y otro entero si lo ha habido
+*/
 static int menus_products(SQLHSTMT *stmt, FILE *out);
+/**
+* menus_orders Implementa la lógica del menu de pedidos
+*
+* @date W.I.P.
+* @author: 
+*
+* @param Puntero a SQLHSTMT
+* @param out Puntero a FILE donde se imprimirá el menu
+* @return Devuelve 0 si no ha habido ningún error y otro entero si lo ha habido
+*/
 static int menus_orders(SQLHSTMT *stmt, FILE *out);
+/**
+* menus_customers Implementa la lógica del menu de clientes
+*
+* @date W.I.P.
+* @author: 
+*
+* @param Puntero a SQLHSTMT
+* @param out Puntero a FILE donde se imprimirá el menu
+* @return Devuelve 0 si no ha habido ningún error y otro entero si lo ha habido
+*/
 static int menus_customers(SQLHSTMT *stmt, FILE *out);
+/**
+* menus_exit Función de control de salida de los menus
+*
+* @date 28-10-2020
+* @author: Erik Yuste
+*
+* @param Puntero a SQLHSTMT
+* @param out Puntero a FILE donde se imprimirá el menu
+*/
 static void menus_exit(FILE *out);
 
 
+
+
+
 /*Products*/
+
+/**
+* menus_productsStock Llama a la función que hace la query 'Stock'
+*
+* @date 29-10-2020
+* @author: Erik Yuste
+*
+* @param Puntero a SQLHSTMT
+* @param out Puntero a FILE donde se imprimirá el resultado
+* @return Devuelve 0 si no ha habido ningún error y otro entero si lo ha habido
+*/
 static int menus_productsStock(SQLHSTMT *stmt, FILE *out);
+/**
+* menus_productsFind Llama a la función que hace la query 'Find'
+*
+* @date 01-11-2020
+* @author: Lucía Martinez-Valero
+*
+* @param Puntero a SQLHSTMT
+* @param out Puntero a FILE donde se imprimirá el resultado
+* @return Devuelve 0 si no ha habido ningún error y otro entero si lo ha habido
+*/
 static int menus_productsFind(SQLHSTMT *stmt, FILE *out);
+
+
+
+
+
+
 
 
 
