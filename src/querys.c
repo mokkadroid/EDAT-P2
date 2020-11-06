a54c36ab818732848219a0732eb60ef5757b41b9
Erik-YO <72315574+Erik-YO@users.noreply.github.com>
2020-11-06 01:33:00 +0100

    Splint 1

--- src/querys.c
+++ src/querys.c
@@ -7,10 +7,10 @@
 
 
 
-void stop();
+static void stop();
 
-void query_productStockInterface(SQLHSTMT *stmt, SQLINTEGER *result, SQLCHAR *productcode);
-void query_productFindInterface(SQLHSTMT *stmt, SQLCHAR *pcode, SQLCHAR *pname, SQLCHAR *string);
+static void query_productStockInterface(SQLHSTMT *stmt, SQLINTEGER *result, SQLCHAR *productcode);
+static void query_productFindInterface(SQLHSTMT *stmt, SQLCHAR *pcode, SQLCHAR *pname, char *string);
 
 
 
@@ -22,14 +22,14 @@ void query_productFindInterface(SQLHSTMT *stmt, SQLCHAR *pcode, SQLCHAR *pname, 
 int query_productStock(SQLHSTMT *stmt, FILE *out){
   SQLRETURN ret; /* ODBC API return status */
   SQLCHAR productcode[MY_CHAR_LEN];
-  SQLINTEGER y;
+  SQLINTEGER y=(SQLINTEGER) 0;
 
   if(!stmt || !out) return 1;
 
 
   fprintf(out," > ");
-  fflush(out);
-  scanf("%s", productcode);
+  if(fflush(out)!=0) printf("ERROR FFLUSH");
+  if(scanf("%s", (char*)productcode)==EOF) printf("ERROR SCANF");
 
   ret=SQLPrepare((*stmt), (SQLCHAR*) "select p.quantityinstock from products as p where p.productcode = ?", SQL_NTS);
   if(!SQL_SUCCEEDED(ret)) printf("ERROR SQLPREPARE %d\n", ret);
@@ -43,15 +43,16 @@ int query_productStock(SQLHSTMT *stmt, FILE *out){
   if(!SQL_SUCCEEDED(ret)) printf("ERROR SQLEXECUTE\n");
 
   /* Asigna la columna resultado a la variable y */
-  ret=SQLBindCol(*stmt, 1, SQL_INTEGER, &y, sizeof(y), NULL);
+  ret=SQLBindCol(*stmt, 1, SQL_INTEGER, &y, (SQLLEN) sizeof(y), NULL);
   if(!SQL_SUCCEEDED(ret)) printf("ERROR SQLBINDCOL\n");
 
   /* Interfaz */
   query_productStockInterface(stmt, &y, productcode);
 
-  SQLCloseCursor(*stmt);
+  ret=SQLCloseCursor(*stmt);
+  if(!SQL_SUCCEEDED(ret)) printf("ERROR SQLCLOSECURSOR\n");
 
-  fflush(out);
+  if(fflush(out)!=0) printf("ERROR FFLUSH");
 
   return 0;
 }
@@ -65,10 +66,10 @@ void query_productStockInterface(SQLHSTMT *stmt, SQLINTEGER *result, SQLCHAR *pr
   int a=0;
 
   while(SQL_SUCCEEDED(ret = SQLFetch(*stmt))) {
-      printf("\n\n < There are %d products with the code \'%s\' >\n\n", *result, productcode);
+      printf("\n\n < There are %d products with the code \'%s\' >\n\n", *result, (char*) productcode);
       a++;
   }
-  if(!a) printf("\n\n < No products with the code \'%s\' found >\n\n",productcode);
+  if(a==0) printf("\n\n < No products with the code \'%s\' found >\n\n",(char*) productcode);
 
   /*stop();*/
 
@@ -86,8 +87,8 @@ void query_productStockInterface(SQLHSTMT *stmt, SQLINTEGER *result, SQLCHAR *pr
 /*select p.productcode, p.productname from products p where p.productname like '%cadena%';*/
 int query_productFind(SQLHSTMT *stmt, FILE *out){
   SQLRETURN ret; /* ODBC API return status */
-  SQLCHAR productcode[MY_CHAR_LEN], productname[MY_CHAR_LEN], string[MY_CHAR_LEN];
-  char query[MY_CHAR_LEN]="select p.productcode, p.productname from products p where p.productname like '%";
+  SQLCHAR productcode[MY_CHAR_LEN], productname[MY_CHAR_LEN];
+  char string[MY_CHAR_LEN], query[MY_CHAR_LEN]="select p.productcode, p.productname from products p where p.productname like '%";
 
 
 
@@ -97,8 +98,8 @@ int query_productFind(SQLHSTMT *stmt, FILE *out){
 
 
   fprintf(out," > ");
-  fflush(out);
-  scanf("%s", string);
+  if(fflush(out)!=0) printf("ERROR FFLUSH");
+  if(scanf("%s", string)==EOF) printf("ERROR SCANF");
 
   strcat(query, string);
   strcat(query, "%' order by p.productcode");
@@ -108,31 +109,32 @@ int query_productFind(SQLHSTMT *stmt, FILE *out){
 
 
   /*ret=SQLBindParameter((*stmt), 1, SQL_PARAM_INPUT, SQL_C_CHAR, SQL_CHAR, 0, 0, string, 0, NULL);
-  if(!SQL_SUCCEEDED(ret)) printf("ERROR SQLBINDPARAMETER\n");
+  if(!SQL_SUCCEEDED(ret)) printf("ERROR SQLBINDPARAMETER\n");*/
 
   /* Ejecuta la query */
   ret=SQLExecute(*stmt);
   if(!SQL_SUCCEEDED(ret)) printf("ERROR SQLEXECUTE\n");
 
   /* Asigna la columna resultado a la variable y */
-  ret=SQLBindCol(*stmt, 1, SQL_CHAR, productcode, sizeof(productcode), NULL);
+  ret=SQLBindCol(*stmt, 1, SQL_CHAR, productcode, (SQLLEN) sizeof(productcode), NULL);
   if(!SQL_SUCCEEDED(ret)) printf("ERROR SQLBINDCOL\n");
   /* Asigna la columna resultado a la variable y */
-  ret=SQLBindCol(*stmt, 2, SQL_CHAR, productname, sizeof(productname), NULL);
+  ret=SQLBindCol(*stmt, 2, SQL_CHAR, productname, (SQLLEN) sizeof(productname), NULL);
   if(!SQL_SUCCEEDED(ret)) printf("ERROR SQLBINDCOL\n");
 
   /* Interfaz */
   query_productFindInterface(stmt, productcode, productname, string);
 
-  SQLCloseCursor(*stmt);
+  ret=SQLCloseCursor(*stmt);
+  if(!SQL_SUCCEEDED(ret)) printf("ERROR SQLCLOSECURSOR\n");
 
-  fflush(out);
+  if(fflush(out)!=0) printf("ERROR FFLUSH");
 
   return 0;
 }
 
 
-void query_productFindInterface(SQLHSTMT *stmt, SQLCHAR *pcode, SQLCHAR *pname, SQLCHAR *string){
+void query_productFindInterface(SQLHSTMT *stmt, SQLCHAR *pcode, SQLCHAR *pname, char *string){
   SQLRETURN ret;
   int a=1;
 
@@ -141,9 +143,9 @@ void query_productFindInterface(SQLHSTMT *stmt, SQLCHAR *pcode, SQLCHAR *pname, 
 
 
   while(SQL_SUCCEEDED(ret = SQLFetch(*stmt))) {
-      printf("   %d\t| %s\t| %s\t\n", a, pcode, pname);
+      printf("   %d\t| %s\t| %s\t\n", a, (char*) pcode, (char*) pname);
       a++;
-      if(!(a%10)){
+      if((a%10)==0){
 
         stop();
         printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\t| Product code\t| Product name\n");
@@ -151,7 +153,7 @@ void query_productFindInterface(SQLHSTMT *stmt, SQLCHAR *pcode, SQLCHAR *pname, 
       }
   }
   printf("\n");
-  if(!(a-1)) printf("\n < No product named \'%s\' >\n\n",string);
+  if(a==1) printf("\n < No product named \'%s\' >\n\n",string);
 
   printf(" < Press ENTER to exit >\n");
 
@@ -183,9 +185,9 @@ void query_productFindInterface(SQLHSTMT *stmt, SQLCHAR *pcode, SQLCHAR *pname, 
 
 
 void stop(){
-
-  while(getchar()!='\n');
-  getchar();
+  int a;
+  while(getchar()!= (int)'\n');
+  a = getchar();
 
   return;
 }
