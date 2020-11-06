bd1c5d1338489e761a9595ea0bebdfb3cafb43b4
Erik-YO <72315574+Erik-YO@users.noreply.github.com>
2020-11-06 09:43:15 +0100

    Comentarios

--- include/menus.h
+++ include/menus.h
@@ -1,3 +1,12 @@
+/**
+ * @brief It defines the menus functions
+ *
+ * @file menus.h
+ * @author Erik Yuste & Lucía Martinez-Valero
+ * @date 28-10-2020
+ */
+
+
 #ifndef MENUS_H
 #define MENUS_H
 
@@ -6,14 +15,16 @@
 #include <sqlext.h>
 
 
+/**
+ * menus_general Implemeta la lógica del menu del que dependen los demás
+ *
+ * @date 28-10-2020
+ * @author: Erik Yuste
+ *
+ * @param game Puntero a SQLHSTMT
+ * @return Devuelve 0 si no ha habido ningún problema y otro entero si lo ha habido
+ */
 int menus_general(SQLHSTMT *stmt, FILE *out);
-
-
-
-
-
-
-
 
 
 
